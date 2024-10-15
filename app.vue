898a47134a50c75a2f2a5f1647f5d75c86b62090
adamswan <melyling@sina.com>
2024-10-15 10:53:51 +0800

    集成sass和配置postcss

--- app.vue
+++ app.vue
@@ -10,14 +10,20 @@
 <script setup lang="ts">
 // 设置seo信息
 useSeoMeta({
-  title: '哔哩哔哩 (゜-゜)つロ 干杯~-bilibili',
-  description: '哔哩哔哩（bilibili.com)是国内知名的视频弹幕网站，这里有及时的动漫新番，活跃的ACG氛围，有创意的Up主。. 大家可以在这里找到许多欢乐。.',
-  keywords: '哔哩哔哩,哔哩哔哩官网,哔哩哔哩动画,哔哩哔哩番剧,哔哩哔哩动画官网,哔哩哔哩番剧官网,哔哩哔哩动画官网,哔哩哔哩番剧官网,哔哩哔哩动画官网,哔哩哔哩番剧官网'
-})
-
+  title: "哔哩哔哩 (゜-゜)つロ 干杯~-bilibili",
+  description:
+    "哔哩哔哩（bilibili.com)是国内知名的视频弹幕网站，这里有及时的动漫新番，活跃的ACG氛围，有创意的Up主。. 大家可以在这里找到许多欢乐。.",
+  keywords:
+    "哔哩哔哩,哔哩哔哩官网,哔哩哔哩动画,哔哩哔哩番剧,哔哩哔哩动画官网,哔哩哔哩番剧官网,哔哩哔哩动画官网,哔哩哔哩番剧官网,哔哩哔哩动画官网,哔哩哔哩番剧官网",
+});
 </script>
 
-<style>
+<style lang="scss">
+// 基础样式
+@import "./assets/styles/base.scss";
+// 字体图标
+@import "./assets/styles/iconfont.scss";
+
 :root {
   /* 覆盖各类颜色的自定义属性 */
   --van-primary-color: #fb7299 !important;
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,4 +7,12 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   // 应用模块
   modules: ["@vant/nuxt"],
+  // 移动端适配
+  postcss: {
+    plugins: {
+      "postcss-px-to-viewport": {
+        viewportWidth: 375,
+      },
+    },
+  },
 });
