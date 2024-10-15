b4547d771f9707796cb68cb298a416860ef018b6
adamswan <melyling@sina.com>
2024-10-15 15:26:05 +0800

    feat: 动态路由获取视频id

---
+++ pages/video/[id].vue
@@ -0,0 +1,14 @@
+<template>
+  <p>视频:{{ videoId }}</p>
+</template>
+    
+<script setup lang='ts'>
+const route = useRoute()
+const videoId = route.params.id
+console.log('videoId', videoId)
+    
+</script>
+    
+<style>
+    
+</style>
